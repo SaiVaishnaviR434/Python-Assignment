ccc3fb9538d1e2f74a998d14c4d9ee50194952a6
Sai Vaishnavi Ravula <152798109+SaiVaishnaviR434@users.noreply.github.com>
2025-05-29 21:57:11 +0530

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,27 @@
+a = [1, 2, 3,4]
+try:
+    print ("Second element = ",a[1])
+    print ("Fourth element = ",a[3])
+    
+except:
+    print ("An error occurred")
+
+print()
+
+b = [3,2,1]
+try:
+    a == b
+except:
+    print("They are not equal")
+else:
+    print("Both Equal") 
+
+print()
+
+try:
+    k = 5/0
+    print(k)
+except ZeroDivisionError:
+    print("Can't divide by zero")
+finally:
+    print('This is always executed') 
